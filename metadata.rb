c9af558b0fe5ce6ca2924b473759c699f90ac235
Fletcher Nichol <fnichol@nichol.ca>
2011-08-11 18:17:26 -0600

    git git, initial commit.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Fletcher Nichol"
+maintainer_email "fnichol@nichol.ca"
+license          "Apache 2.0"
+description      "LWRP to install Mac OS X applications from zip archives"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+supports "mac_os_x"
