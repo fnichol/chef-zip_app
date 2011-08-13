96285a810d9ed2cce425f1cb1d64afadc769b7b2
Fletcher Nichol <fnichol@nichol.ca>
2011-08-13 15:08:25 -0600

    Update metadata.

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.1"
 
 supports "mac_os_x"
+
+recipe "zip_app", "Processes a list of *zip_apps* (which is emtpy by default) to be installed."
+recipe "zip_app::data_bag", "Fetches an list of *zip_apps* from a data bag item and appends it to the `node['zip_app']['apps']` attribute for processing."
